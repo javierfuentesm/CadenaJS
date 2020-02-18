bc9b11b101516e201d622d08ccd231e164479017
rjfm <javier.fuentesm@hotmail.com>
2020-02-18 08:18:25 -0600

    Done

---
+++ cadena.js
@@ -0,0 +1,12 @@
+const chars = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var final = "";
+var t0 = performance.now();
+for (let index = 0; index < 26 * 26 * 26; index++) {
+  final +=
+    [...Array(3)].map(i => chars[(Math.random() * chars.length) | 0]).join`` +" ";
+}
+
+console.log((final.match(/IPN/g) || []).length);
+
+var t1 = performance.now();
+console.log("Tomo " + (t1 - t0) + " milliseconds.");
