bcf6044dff331210de992369a3efc95c79fb967a
rjfm <javier.fuentesm@hotmail.com>
2020-02-19 07:41:25 -0600

    Segundo programa

---
+++ while.js
@@ -0,0 +1,15 @@
+var t0 = performance.now();
+
+var max = 100000.0;
+var seno,cose,tang,logn,raiz;
+var i = 0.0;
+while(i < max){
+    seno += Math.sin(i);
+    cose += Math.cos(i);
+    tang += Math.tan(i);
+    logn += Math.log(i);
+    raiz += Math.sqrt(i);
+    i++;
+}
+var t1 = performance.now();
+console.log("Tomo " + (t1 - t0) + " milliseconds.");
